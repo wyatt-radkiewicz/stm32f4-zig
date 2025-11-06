907614a12548148b904ce72260d36b0f978b9f28
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2025-11-05 17:24:18 -0700

    Added debugging functionality

--- build.zig
+++ build.zig
@@ -4,7 +4,8 @@ const arm = std.Target.arm;
 const config = @import("build.zig.zon");
 
 pub fn build(b: *std.Build) void {
-    // Setup common configuration
+    // Setup common configuration and build options
+    const host = b.standardTargetOptions(.{});
     const target = b.resolveTargetQuery(.{
         .cpu_arch = .thumb,
         .os_tag = .freestanding,
@@ -13,11 +14,12 @@ pub fn build(b: *std.Build) void {
         .cpu_features_add = arm.featureSet(&.{ arm.Feature.dsp, arm.Feature.strict_align }),
     });
     const optimize = b.standardOptimizeOption(.{});
+    const gdb_path = b.option([]const u8, "gdb", "Path to gdb for the debug step");
 
     // Gather build steps
     const install_step = b.getInstallStep();
-    const build_step = b.step("build", "Build the main elf executable file");
-    const flash_step = b.step("flash", "Build, flash, and run the code on the stm32f4discovery");
+    const run_step = b.step("run", "Run the code on the stm32f4discovery");
+    const debug_step = b.step("debug", "Debug the code on the stm32f4discovery");
     const fmt_step = b.step("fmt", "Check formatting");
     const clean_step = b.step("clean", "Cleanup cached files");
 
@@ -35,7 +37,6 @@ pub fn build(b: *std.Build) void {
         .version = std.SemanticVersion.parse(config.version) catch @panic("Bad semver format!"),
     });
     main_exe.setLinkerScript(b.path(b.pathJoin(&.{ "scripts", "linker.ld" })));
-    build_step.dependOn(&main_exe.step);
     install_step.dependOn(&b.addInstallArtifact(main_exe, .{}).step);
 
     // Flash image to the device
@@ -43,8 +44,33 @@ pub fn build(b: *std.Build) void {
     openocd_run.addArgs(&.{ "-f", "interface/stlink.cfg" });
     openocd_run.addArgs(&.{ "-f", "board/stm32f4discovery.cfg", "-c" });
     openocd_run.addPrefixedArtifactArg("set img_name ", main_exe);
-    openocd_run.addArgs(&.{ "-c", "init; program \"$img_name\" verify reset exit" });
-    flash_step.dependOn(&openocd_run.step);
+    openocd_run.addArgs(&.{ "-c", "init; program \"$img_name\" preverify verify reset exit" });
+    run_step.dependOn(&openocd_run.step);
+
+    // Debug step
+    if (gdb_path) |path| {
+        // Debugger module
+        const debug_mod = b.createModule(.{
+            .root_source_file = b.path(b.pathJoin(&.{ "scripts", "debug.zig" })),
+            .optimize = optimize,
+            .target = host,
+        });
+
+        // Compile the debugger
+        const debug_exe = b.addExecutable(.{
+            .name = "debugger",
+            .root_module = debug_mod,
+        });
+
+        // Run the debugger
+        const debug_run = b.addRunArtifact(debug_exe);
+        debug_run.addArg(path);
+        debug_run.addArtifactArg(main_exe);
+        debug_run.step.dependOn(&openocd_run.step);
+        debug_step.dependOn(&debug_run.step);
+    } else {
+        debug_step.dependOn(&b.addFail("Debug step requires -Dgdb option to be set").step);
+    }
 
     // Format step
     const fmt = b.addFmt(.{
---
+++ scripts/debug.zig
@@ -0,0 +1,75 @@
+//! Small debugger script
+const std = @import("std");
+
+/// Prints usage message
+fn usage(stdout: *std.io.Writer, program: []const u8) !void {
+    try stdout.print(
+        \\usage:
+        \\\t{s} <arm eabi compatible gdb> <elf file>
+    , .{program});
+}
+
+/// Trys to debug the device
+fn debug(
+    allocator: std.mem.Allocator,
+    stdout: *std.io.Writer,
+    prg_name: []const u8,
+    args: *std.process.ArgIterator,
+) !void {
+    // Main settings and config
+    const gdb_path = args.next() orelse {
+        try usage(stdout, prg_name);
+        return error.InvalidArgs;
+    };
+    const elf_path = args.next() orelse {
+        try usage(stdout, prg_name);
+        return error.InvalidArgs;
+    };
+
+    // Start up openocd server, halting first
+    var openocd = std.process.Child.init(&.{
+        "openocd",
+        "-f",
+        "interface/stlink.cfg",
+        "-f",
+        "board/stm32f4discovery.cfg",
+        "-c",
+        "init; reset halt",
+    }, allocator);
+    openocd.stdout_behavior = .Ignore;
+    openocd.stdin_behavior = .Ignore;
+    openocd.stderr_behavior = .Ignore;
+    try openocd.spawn();
+
+    // Start up gdb
+    var gdb = std.process.Child.init(&.{
+        gdb_path,
+        elf_path,
+        "-ex",
+        "target remote localhost:3333",
+    }, allocator);
+    gdb.stdout_behavior = .Inherit;
+    gdb.stdin_behavior = .Inherit;
+    gdb.stderr_behavior = .Inherit;
+    _ = try gdb.spawnAndWait();
+
+    // Shutdown openocd server
+    _ = try openocd.kill();
+}
+
+/// Runs openocd in the background, runs the debugger in the foreground
+pub fn main() !void {
+    // Get our allocator and stdout
+    const allocator = std.heap.smp_allocator;
+    var buffer: [128]u8 = undefined;
+    const writer = std.fs.File.stdout().writer(&buffer);
+    var stdout = writer.interface;
+
+    // Read in our argument list and figure out what to do
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+    const prg_name = args.next() orelse @panic("Expected program name as first argument!");
+
+    // Run the debug code
+    try debug(allocator, &stdout, prg_name, &args);
+}
